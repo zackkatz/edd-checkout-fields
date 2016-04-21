592116ab3248ae4d9d5d884b4e02e1445b1a1472
Pippin Williamson <pippin@pippinsplugins.com>
2016-04-21 14:47:32 -0500

    Version bump, changelog, updated lang files for 2.0.4

--- edd-cfm.php
+++ edd-cfm.php
@@ -6,7 +6,7 @@
  * Author:              Chris Christoff
  * Author URI:          http://www.chriscct7.com
  *
- * Version:             2.0.3
+ * Version:             2.0.4
  * Requires at least:   4.3
  * Tested up to:        4.5
  *
@@ -320,7 +320,7 @@ class EDD_Checkout_Fields_Manager {
 		}
 		// Plugin Version
 		if ( !defined( 'cfm_plugin_version' ) ) {
-			define( 'cfm_plugin_version', '2.0.3' );
+			define( 'cfm_plugin_version', '2.0.4' );
 		}
 		// Plugin Root File
 		if ( !defined( 'cfm_plugin_file' ) ) {
